606c548da7f89506b39f8666ad6fa1b6a8daf828
Emil <emil@personalkollen.se>
2024-07-18 13:23:37 +0200

    Add some tests

---
+++ tests/requests.py
@@ -0,0 +1,28 @@
+import respx
+import pytest
+from httpx import Response
+from blocket_api.blocket import BASE_URL, APIError, BlocketAPI, _make_request
+
+api = BlocketAPI("token")
+
+
+def test_make_request_no_raise():
+    _make_request(url=f"{BASE_URL}/not_found", token="token", raise_for_status=False)
+
+
+@respx.mock
+def test_make_request_raise_404():
+    respx.get(f"{BASE_URL}/not_found").mock(
+        return_value=Response(status_code=404),
+    )
+    with pytest.raises(APIError):
+        _make_request(url=f"{BASE_URL}/not_found", token="token", raise_for_status=True)
+
+
+@respx.mock
+def test_make_request_raise_401():
+    respx.get(f"{BASE_URL}/unauthorized").mock(
+        return_value=Response(status_code=401),
+    )
+    with pytest.raises(APIError):
+        _make_request(url=f"{BASE_URL}/unauthorized", token="token")
