37e046ee6d4912c3ab3f441f8077b9073cc1d480
dunderrrrrr <emil.bjorkroth@proton.me>
2026-09-07 19:50:01 +0200

    fix(bs4): find() deprecation warning
    DeprecationWarning: The 'text' argument to find()-type methods is deprecated. Use 'string' instead.

--- blocket_api/ad_parser.py
+++ blocket_api/ad_parser.py
@@ -193,7 +193,7 @@ class McAd(MobilityAd):
 
     def extend(self, data: dict, soup: BeautifulSoup, grid: Tag) -> None:
         if place_h2 := soup.find(
-            "h2", text=lambda t: isinstance(t, NavigableString) and "Plats" in t
+            "h2", string=lambda t: isinstance(t, NavigableString) and "Plats" in t
         ):
             if link := place_h2.find_parent():
                 data["location"] = link.get_text(strip=True)
